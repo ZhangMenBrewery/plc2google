e04adbfa942de2e0e7b79434e19e3e599e5b0fd2
chunkai721 <chunkai721@gmail.com>
2021-12-24 16:56:27 +0800

    20211224

--- plc2google.py
+++ plc2google.py
@@ -1,3 +1,6 @@
+import mysql.connector
+from mysql.connector import Error
+from pyasn1.type.univ import Null
 import pygsheets
 import time
 import datetime
@@ -15,6 +18,13 @@ class zmb_plc():
         self.__zmb_group = [
             'zhangmenbrewery@gmail.com', 'chunkai721@gmail.com']
         self.__plc_tag = {}
+
+        self.__sql_host = 'localhost'
+        self.__sql_user = 'zhangmen'
+        self.__sql_password = '54685508'
+        path = 'sql_insert.json'
+        with open(path,encoding='utf-8', errors='ignore') as f:
+            self.__sql_insert_tag = json.load(f, strict=False)
 
     # ss mean spreadsheet
     def open_ss(self):
@@ -84,17 +94,77 @@ class zmb_plc():
                     pass
             wks.update_values(f"A{rows}:{dict(zip(range(1,27),string.ascii_uppercase))[len(self.__plc_tag[item])+1]}{rows}", [data, ])    
 
+        #建立sql連線
+    def connect_db(self, db):
+        connection = None
+        try:
+            connection = mysql.connector.connect(
+                host=self.__sql_host,
+                user=self.__sql_user,
+                passwd=self.__sql_password,
+                database=db
+            )
+            print("Connection to MySQL DB successful")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+        return connection
+
+    def write_to_sheet_sql(self,ss):
+        client = snap7.client.Client()
+        client.connect("192.168.60.201", 0, 2)
+    #    plc_di = client.eb_read(0, 7)
+    #    print(snap7.util.get_bool(plc_di,6,1))
+    #    plc_do = client.ab_read(0 ,11)
+    #    print(snap7.util.get_bool(plc_do, 10, 5))
+
+        conn = self.connect_db("zmb")
+        cur = conn.cursor()
+
+        for item in self.__plc_tag.keys():
+            print(f"{time.strftime('%m/%d/%Y %H:%M:%S', time.localtime())} > {item}")
+            wks = ss.worksheet_by_title(item)
+            rows = len(wks.get_col(1, include_tailing_empty=False))+1
+            data = []
+            data.append(time.strftime('%m/%d/%Y %H:%M:%S', time.localtime()))
+            for value in self.__plc_tag[item].values():
+                tag = value.split('.')
+                if tag[0].startswith('DB'):
+                    if tag[1].startswith('DBDDI'):#db client.db_read(int(tag[0][2:]), int(tag[1][3:]), 4)
+                        data.append(snap7.util.get_dint(client.db_read(int(tag[0][2:]), int(tag[1][5:]), 4), 0))  # DB40.DBD8
+                    elif tag[1].startswith('DBD'):#db client.db_read(int(tag[0][2:]), int(tag[1][3:]), 4)
+                        data.append(snap7.util.get_real(client.db_read(int(tag[0][2:]), int(tag[1][3:]), 4), 0))  # DB40.DBD8
+                    elif tag[1].startswith('DBX'):
+                        data.append(1 if snap7.util.get_bool(client.db_read(int(tag[0][2:]), int(tag[1][3:]), 1), 0,int(tag[2])) == True else 0)  # DB40.DBX3.3
+                    else:
+                        pass
+                elif tag[0].startswith('Q'):
+                    data.append(1 if snap7.util.get_bool(client.ab_read(0, 11), int(tag[0][1:]), int(tag[1])) == True else 0)  # Q4.3
+                else: # for digital input
+                    pass
+            wks.update_values(f"A{rows}:{dict(zip(range(1,27),string.ascii_uppercase))[len(self.__plc_tag[item])+1]}{rows}", [data, ])
+            sql_insert = self.__sql_insert_tag[item]
+            # data.insert(0,'')
+            # print(data)
+            data[0] = datetime.datetime.strptime(data[0], '%m/%d/%Y %H:%M:%S')
+            data[0] = data[0].strftime('%Y-%m-%d %H:%M:%S')
+            if str(item).startswith('FV'):
+                data = data[:-1]
+            cur.execute(sql_insert, tuple(data))
+            conn.commit()
+        cur.close()
+        conn.close()
+
 def main():
-        while True:
-            try:
-                plc_obj = zmb_plc()
-                ss_obj = plc_obj.open_ss()
-                plc_obj.write_to_sheet(ss_obj)
-            except Exception as error:
-                print(f"{time.strftime('%m/%d/%Y %H:%M:%S', time.localtime())} > {error} ")
-                time.sleep(60)
-            print(f"{time.strftime('%m/%d/%Y %H:%M:%S', time.localtime())} > Sleeping 300sec")
-            time.sleep(300)
+        # while True:
+        try:
+            plc_obj = zmb_plc()
+            ss_obj = plc_obj.open_ss()
+            plc_obj.write_to_sheet_sql(ss_obj)
+        except Exception as error:
+            print(f"{time.strftime('%m/%d/%Y %H:%M:%S', time.localtime())} > {error} ")
+            time.sleep(60)
+        print(f"{time.strftime('%m/%d/%Y %H:%M:%S', time.localtime())} > Sleeping 300sec")
+        time.sleep(300)
 
 if __name__ == '__main__':
     main()
